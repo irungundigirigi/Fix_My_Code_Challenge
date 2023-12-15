3d975eded9739197b30ca6ef89b406572ec509d5
irungundigirigi <irungundigirigi@gmail.com>
2023-12-15 23:19:33 +0300

    update code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,13 +2,34 @@
 
 class square():
     
-    width = 0
-    height = 0
-
+    def __init__(self, width=0, height=0):
+        """ Initialization """
+        self.width = width
+        self.height = height
     
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    @property
+    def width(self):
+        """ Width getter """
+        return self.__width
+
+    @property
+    def height(self):
+        """ Height getter """
+        return self.__height
+    @width.setter
+    def width(self, value):
+        """ Width setter """
+        if value <= 0:
+            raise ValueError('Width must be greator than zero')
+        else:
+            self.__width = value
+    @height.setter
+    def height(self, value):
+        """ Height setter """
+        if value <= 0:
+            raise ValueError('Height must be greator than zero')
+        else:
+            self.__height = value 
 
     def area_of_my_square(self):
         """ Area of the square """
